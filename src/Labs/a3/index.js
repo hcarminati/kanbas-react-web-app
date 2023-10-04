e579e92a3d8bf37a38a2d95ae59914a0c764a65e
hcarminati <hscarminati@hotmail.com>
2023-10-04 14:39:56 -0400

    a3 up until 3.3

---
+++ src/Labs/a3/index.js
@@ -0,0 +1,14 @@
+import HelloWorld from "./HelloWorld";
+import Kanbas from "../../Kanbas";
+
+function Assignment3() {
+    return (
+        <div className="container">
+            <h1>Assignment 3</h1>
+            <HelloWorld/>
+            <Kanbas/>
+        </div>
+    );
+}
+
+export default Assignment3;
